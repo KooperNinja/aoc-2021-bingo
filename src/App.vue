67ddfde6cd168ea9d586263dd61e2726e2dbec39
KooperNinja <sven@schraeer.com>
2023-11-30 19:56:55 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { Ref, ref } from 'vue';
+import BingoBoard from './components/BingoBoard.vue';
+
+class Field {
+  num : number = 0
+  highlighted : boolean = false
+
+  constructor(num : number) {
+    this.num = num
+  }
+}
+
+interface Board {
+  fields : Field[]
+}
+
+const draw = ref([2, 24])
+const boards : Ref<Board[]> = ref({fields: [new Field(2)]})
+
+export type { Field }
+</script>
+  
+<template>
+  <div v-for="board in boards">
+    <BingoBoard :fields="board.fields"></BingoBoard>
+  </div>
+  
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/BingoBoard.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Field } from '../App.vue'
+
+interface Props {
+  fields : Field[]
+}
+
+const props = defineProps<Props>()
+
+const fields = ref(props.fields)
+</script>
+
+<template>
+  <h1>Hello</h1>
+
+  <div class="grid-container">
+    <div v-for="field in fields" class="grid-item">
+      {{ field.num }}
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
